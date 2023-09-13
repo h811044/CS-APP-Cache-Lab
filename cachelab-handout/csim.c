8220932eab56ef49a53faaa83b34b4ab5fe87f49
h811044 <142477229+h811044@users.noreply.github.com>
2023-09-13 11:47:35 +0800

    Handins

--- cachelab-handout/csim.c
+++ cachelab-handout/csim.c
@@ -1,7 +1,147 @@
 #include "cachelab.h"
+#include <stdio.h>
+#include <getopt.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <assert.h>
 
-int main()
+typedef struct entry {
+    long long int tag;
+    int touch;
+    int valid;
+} Entry;
+
+int main(int argc, char *argv[])
 {
-    printSummary(0, 0, 0);
+    /* parse the command line arguments */
+    int opt, v = 0, s = 0, E = 0, b = 0;
+    int S;
+    char *t = NULL;
+
+    while ((opt = getopt(argc, argv, "hvs:E:b:t:")) != -1) {
+        switch (opt) {
+            case 'h':
+                printf("./csim -s 4 -E 1 -b 4 -t traces/yi.trace\n");
+                printf("./csim -v -s 4 -E 1 -b 4 -t traces/yi.trace\n");
+                break;
+            case 'v':
+                v = 1;
+                break;
+            case 's':
+                s = atoi(optarg);
+                break;
+            case 'E':
+                E = atoi(optarg);
+                break;
+            case 'b':
+                b = atoi(optarg);
+                break;
+            case 't':
+                t = malloc(strlen(optarg) + 1);
+                strcpy(t, optarg);
+                break;
+            default:
+                printf("getopt err");
+                exit(EXIT_FAILURE);
+        }
+    }
+
+    S = 1 << s;
+    // printf("v = %d, s = %d, E = %d, b = %d, t = %s\n", v, s, E, b, t);
+
+    /* allocate storage for the simulator's data structures */
+    Entry *table;
+
+    table = (Entry *)calloc(S * E , sizeof(Entry));
+    assert(table != NULL);
+
+    /* parse the trace */
+    FILE *fp;
+
+    if (!(fp = fopen(t, "r"))) {
+        printf("fopen err %s\n", t);
+        exit(EXIT_FAILURE);
+    }
+
+    int hit_count = 0, miss_count = 0, eviction_count = 0;
+    int tag_shift = s + b;
+    int set_shift = b;
+    int set_mask = S - 1;
+    int touch = 0;
+
+    char operation;
+    long long int address;
+    int size;
+    long long int tag;
+    int set;
+
+    while ((fscanf(fp, " %c %llx,%d", &operation, &address, &size)) != EOF) {
+        if (operation == 'I') {
+            continue;
+        } else if (operation == 'L' || operation == 'S' || operation == 'M') {
+            tag = address >> tag_shift;
+            set = (address >> set_shift) & set_mask;
+
+            int hit = 0;
+            int target_index, target_touch;
+
+            target_touch = ++touch;
+
+            for (int index = set * E; index < (set + 1) * E; index++) {
+                if (table[index].tag == tag && table[index].valid == 1) {
+                    hit = 1;
+                    target_index = index;
+                    break;
+                } else {
+                    if (table[index].touch < target_touch) {
+                        target_index = index;
+                        target_touch = table[index].touch;
+                    }
+                }
+            }
+
+            if (v) {
+                printf("%c %llx,%d", operation, address, size);
+            }
+
+            if (hit) {
+                hit_count++;
+                if (v) {
+                    printf(" hit");
+                }
+            } else {
+                miss_count++;
+                if (v) {
+                    printf(" miss");
+                }
+                if (table[target_index].valid) {
+                    eviction_count++;
+                    if (v) {
+                        printf(" eviction");
+                    }
+                }
+                table[target_index].tag = tag;
+                table[target_index].valid = 1;
+            }
+            table[target_index].touch = touch;
+
+            if (operation == 'M') {
+                hit_count++;
+                if (v) {
+                    printf(" hit");
+                }
+            }
+
+            if (v) {
+                printf(" \n");
+            }
+        } else {
+            printf("fscanf err %c\n", operation);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    printSummary(hit_count, miss_count, eviction_count);
     return 0;
 }
--- cachelab-handout/trans.c
+++ cachelab-handout/trans.c
@@ -10,7 +10,115 @@
 #include <stdio.h>
 #include "cachelab.h"
 
+#define BLOCKING_SIZE_32_32 8   /* must be a factor of 32 */
+#define STRIDE_32_32 8  /* must be a factor of BLOCKING_SIZE_32_32 */
+#define BLOCKING_SIZE_64_64_M 4 /* must be a factor of 64 */
+#define BLOCKING_SIZE_64_64_N 8 /* must be a factor of 64 */
+#define STRIDE_64_64 4  /* must be a factor of BLOCKING_SIZE_64_64_M */
+#define BLOCKING_SIZE_61_67 23
+#define BLOCKING_SIZE_DEF 1
+
 int is_transpose(int M, int N, int A[N][M], int B[M][N]);
+
+void transpose_32_32(int M, int N, int A[N][M], int B[M][N])
+{
+    int i, j, k, l;
+    int tmp0, tmp1, tmp2, tmp3, tmp4, tmp5, tmp6, tmp7; /* stride 8 */
+    M = M / BLOCKING_SIZE_32_32;
+    N = N / BLOCKING_SIZE_32_32;
+
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < M; j++) {
+            for (k = 0; k < BLOCKING_SIZE_32_32; k++) {
+                for (l = 0; l < BLOCKING_SIZE_32_32; l+=STRIDE_32_32) {
+                    tmp0 = A[i*BLOCKING_SIZE_32_32+k][j*BLOCKING_SIZE_32_32+l];
+                    tmp1 = A[i*BLOCKING_SIZE_32_32+k][j*BLOCKING_SIZE_32_32+l+1];
+                    tmp2 = A[i*BLOCKING_SIZE_32_32+k][j*BLOCKING_SIZE_32_32+l+2];
+                    tmp3 = A[i*BLOCKING_SIZE_32_32+k][j*BLOCKING_SIZE_32_32+l+3];
+                    tmp4 = A[i*BLOCKING_SIZE_32_32+k][j*BLOCKING_SIZE_32_32+l+4];
+                    tmp5 = A[i*BLOCKING_SIZE_32_32+k][j*BLOCKING_SIZE_32_32+l+5];
+                    tmp6 = A[i*BLOCKING_SIZE_32_32+k][j*BLOCKING_SIZE_32_32+l+6];
+                    tmp7 = A[i*BLOCKING_SIZE_32_32+k][j*BLOCKING_SIZE_32_32+l+7];
+                    B[j*BLOCKING_SIZE_32_32+l][i*BLOCKING_SIZE_32_32+k] = tmp0;
+                    B[j*BLOCKING_SIZE_32_32+l+1][i*BLOCKING_SIZE_32_32+k] = tmp1;
+                    B[j*BLOCKING_SIZE_32_32+l+2][i*BLOCKING_SIZE_32_32+k] = tmp2;
+                    B[j*BLOCKING_SIZE_32_32+l+3][i*BLOCKING_SIZE_32_32+k] = tmp3;
+                    B[j*BLOCKING_SIZE_32_32+l+4][i*BLOCKING_SIZE_32_32+k] = tmp4;
+                    B[j*BLOCKING_SIZE_32_32+l+5][i*BLOCKING_SIZE_32_32+k] = tmp5;
+                    B[j*BLOCKING_SIZE_32_32+l+6][i*BLOCKING_SIZE_32_32+k] = tmp6;
+                    B[j*BLOCKING_SIZE_32_32+l+7][i*BLOCKING_SIZE_32_32+k] = tmp7;
+                }
+            }
+        }
+    }
+}
+
+void transpose_64_64(int M, int N, int A[N][M], int B[M][N])
+{
+    /*
+     *  4   4   4   4   4   4   4   4   5   5   5   5   ...
+     *  12  12  12  12  12  12  12  12  13  13  13  13  ...
+     *  20  20  20  20  20  20  20  20  21  21  21  21  ...
+     *  28  28  28  28  28  28  28  28  29  29  29  29  ...
+     *  4   4   4   4   ...
+     *  12  ...
+     *  ...
+     */
+
+    int i, j, k, l;
+    int tmp0, tmp1, tmp2, tmp3; /* stride 4 */
+    int tmp4, tmp5, tmp6, tmp7; /* stride 4 */
+    M = M / BLOCKING_SIZE_64_64_M;
+    N = N / BLOCKING_SIZE_64_64_N;
+
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < M; j++) {
+            for (k = 0; k < BLOCKING_SIZE_64_64_N/2; k++) {
+                for (l = 0; l < BLOCKING_SIZE_64_64_M; l+=STRIDE_64_64) {
+                    tmp0 = A[i*BLOCKING_SIZE_64_64_N+k][j*BLOCKING_SIZE_64_64_M+l];
+                    tmp1 = A[i*BLOCKING_SIZE_64_64_N+k][j*BLOCKING_SIZE_64_64_M+l+1];
+                    tmp2 = A[i*BLOCKING_SIZE_64_64_N+k][j*BLOCKING_SIZE_64_64_M+l+2];
+                    tmp3 = A[i*BLOCKING_SIZE_64_64_N+k][j*BLOCKING_SIZE_64_64_M+l+3];
+                    tmp4 = A[i*BLOCKING_SIZE_64_64_N+k+4][j*BLOCKING_SIZE_64_64_M+l];
+                    tmp5 = A[i*BLOCKING_SIZE_64_64_N+k+4][j*BLOCKING_SIZE_64_64_M+l+1];
+                    tmp6 = A[i*BLOCKING_SIZE_64_64_N+k+4][j*BLOCKING_SIZE_64_64_M+l+2];
+                    tmp7 = A[i*BLOCKING_SIZE_64_64_N+k+4][j*BLOCKING_SIZE_64_64_M+l+3];
+                    B[j*BLOCKING_SIZE_64_64_M+l][i*BLOCKING_SIZE_64_64_N+k] = tmp0;
+                    B[j*BLOCKING_SIZE_64_64_M+l+1][i*BLOCKING_SIZE_64_64_N+k] = tmp1;
+                    B[j*BLOCKING_SIZE_64_64_M+l+2][i*BLOCKING_SIZE_64_64_N+k] = tmp2;
+                    B[j*BLOCKING_SIZE_64_64_M+l+3][i*BLOCKING_SIZE_64_64_N+k] = tmp3;
+                    B[j*BLOCKING_SIZE_64_64_M+l][i*BLOCKING_SIZE_64_64_N+k+4] = tmp4;
+                    B[j*BLOCKING_SIZE_64_64_M+l+1][i*BLOCKING_SIZE_64_64_N+k+4] = tmp5;
+                    B[j*BLOCKING_SIZE_64_64_M+l+2][i*BLOCKING_SIZE_64_64_N+k+4] = tmp6;
+                    B[j*BLOCKING_SIZE_64_64_M+l+3][i*BLOCKING_SIZE_64_64_N+k+4] = tmp7;
+                }
+            }
+        }
+    }
+}
+
+void transpose_default(int M, int N, int A[N][M], int B[M][N], int blocking_size)
+{
+    int i, j, k, l;
+    int tmp;    /* stride 1 */
+    int m, n;
+    m = M / blocking_size + 1;
+    n = N / blocking_size + 1;
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < m; j++) {
+            for (k = 0; k < blocking_size; k++) {
+                for (l = 0; l < blocking_size; l++) {
+                    if (i*blocking_size+k >= N || j*blocking_size+l >= M) {
+                        continue;
+                    }
+                    tmp = A[i*blocking_size+k][j*blocking_size+l];
+                    B[j*blocking_size+l][i*blocking_size+k] = tmp;
+                }
+            }
+        }
+    }
+}
 
 /* 
  * transpose_submit - This is the solution transpose function that you
@@ -22,6 +130,18 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
+    if (M == 32 && N == 32) {
+        /* TEST_TRANS_RESULTS=1:287 */
+        transpose_32_32(M, N, A, B);
+    } else if (M == 64 && N == 64) {
+        /* TEST_TRANS_RESULTS=1:1587 */
+        transpose_64_64(M, N, A, B);
+    } else if (M == 61 && N == 67) {
+        /* TEST_TRANS_RESULTS=1:1928 */
+        transpose_default(M, N, A, B, BLOCKING_SIZE_61_67);
+    } else {
+        transpose_default(M, N, A, B, BLOCKING_SIZE_DEF);
+    }
 }
 
 /* 
